b0c4900b52110cdc56db95305d2ba653ea373d59
Emjay <abdulmujeebj@gmail.com>
2019-01-16 20:27:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Container, Header, Divider } from 'semantic-ui-react'
+import BikeCard from './Bike';
 import './App.css';
 
 class App extends Component {
+  state = {
+    stations: []
+  }
+
+  componentDidMount() {
+    fetch('https://api.citybik.es/v2/networks/blue-bikes')
+      .then(res => res.json())
+      .then(res => this.setState({ stations: res.network.stations.slice(0, 12) }))
+  }
+
   render() {
+    const { stations } = this.state;
+    const stationMap = stations.map(s => {
+      return <BikeCard key={s.id} bike={s} />
+    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Container text>
+          <Header as='h1'>List of Stations</Header>
+          <Divider/>
+          <div className="ui three doubling stackable cards">
+            {stationMap}
+          </div>
+        </Container>
       </div>
     );
   }
